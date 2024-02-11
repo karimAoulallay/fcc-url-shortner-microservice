def2f4d23aa3b88ed7f0fcc442ade7c6ef6102cf
Abdelkarim Aoulallay <aoulallayabdelkarim@gmail.com>
2024-02-11 12:31:23 +0100

    init commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +9,49 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// parse body request
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlArray = [];
+let shortUrl = 0;
+const errorjSON = { error: "invalid url" };
+const { lookup } = require("dns");
+
+app.post("/api/shorturl", function (req, res) {
+  try {
+    const url = new URL(req.body.url);
+    const regExp = new RegExp(
+      "(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})"
+    );
+    if (!url.href.match(regExp)) {
+      res.json(errorjSON);
+    }
+    lookup(url.hostname, function (err, address) {
+      if (!address) {
+        res.json(errorjSON);
+      } else {
+        const original_url = url.href;
+        urlArray.push({ original_url, short_url: shortUrl++ });
+        res.json(urlArray.at(-1));
+      }
+    });
+  } catch {
+    res.json(errorjSON);
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const paramsShorturl = req.params.shorturl;
+  const url = urlArray.find((url) => url.short_url == paramsShorturl);
+  res.redirect(url.original_url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
